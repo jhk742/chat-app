9783c41ca7db978b873a04b92a2367a9836975eb
jhk742 <jhk742@nyu.edu>
2024-02-06 12:57:33 +0900

    implemented feature to retrieve all users (server)

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -90,4 +90,13 @@ const findUser = async (req, res) => {
     }
 }
 
-module.exports = { registerUser, loginUser, findUser }
+const getUsers = async (req, res) => {
+    try {
+        const users = await userModel.find()
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+}
+module.exports = { registerUser, loginUser, findUser, getUsers }
