59871ca6cb0ab2d6f2540406e39f3bc6f187a9f7
jhk742 <jhk742@nyu.edu>
2024-02-06 14:44:14 +0900

    components and context api setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap'
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <Form>
+            <Row style={{
+                height: "100vh",
+                justifyContent: "center",
+                paddingTop: "20%"
+            }}>
+                <Col xs={6}>
+                    <Stack gap={3}>
+                        <h2>Login</h2>
+                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="password" placeholder="Password" />
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+
+                        <Alert variant="danger">
+                            <span>An error occurred</span>
+                        </Alert>
+                    </Stack>
+                </Col>
+            </Row>
+        </Form>
+    )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap'
 
 export default function Register() {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <Form>
+            <Row style={{
+                height: "100vh",
+                justifyContent: "center",
+                paddingTop: "20%"
+            }}>
+                <Col xs={6}>
+                    <Stack gap={3}>
+                        <h2>Register</h2>
+                        <Form.Control type="text" placeholder="Name" />
+                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="password" placeholder="Password" />
+                        <Button variant="primary" type="submit">
+                            Register
+                        </Button>
+
+                        <Alert variant="danger">
+                            <span>An error occurred</span>
+                        </Alert>
+                    </Stack>
+                </Col>
+            </Row>
+        </Form>
+    )
 }
