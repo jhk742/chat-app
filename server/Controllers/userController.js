6001d72b1ab7f355fc744deef45dc6241e77a356
jhk742 <jhk742@nyu.edu>
2024-02-06 12:23:07 +0900

    initial commit

---
+++ server/Controllers/userController.js
@@ -0,0 +1,57 @@
+const userModel = require("../Models/userModel")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
+const jwt = require("jsonwebtoken")
+
+const createToken = (_id) => {
+    const jwtKey = process.env.JWT_SECRET_KEY
+    return jwt.sign({_id}, jwtKey, {expiresIn: "3d"})
+}
+
+/**
+ * Statuses:
+ * 200: good
+ * 400: user error
+ * 500: server error
+ */
+
+const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body
+
+        //check if user already exists in the db
+        let user = await userModel.findOne({email})
+        if (user)
+            return res.status(400).json("User with the given email already exists.")
+    
+        //validation
+        if (!name || !email || !password) 
+            return res.status(400).json("All fields are required.")
+    
+        //checking to see if the fields are strong enough. IF not...
+        if (!validator.isEmail(email)) 
+            return res.status(400).json("Email must be a valid email.")
+        if (!validator.isStrongPassword(password)) 
+            return res.status(400).json("Password must be a strong password.")
+    
+        //all security conditions passed -> register the user
+        user = new userModel({name, email, password})
+    
+        //hash the password
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(user.password, salt)
+    
+        //register user into db
+        await user.save()
+    
+        //create a token
+        const token = createToken(user._id)
+    
+        res.status(200).json({_id: user._id, name, email, token})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json
+    }
+}
+
+module.exports = {registerUser}
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const userRoute = require("./Routes/userRoute")
+
+const app = express()
+//configure dotenv
+require("dotenv").config()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use("/api/users", userRoute)
+
+/**
+ * CRUD ops
+ * app.post -> create
+ * app.get -> read
+ * app.put -> update
+ * app.delete -> delete
+ */
+app.get("/", (req, res) => {
+    res.send("Welcome to our chat app api")
+})
+
+const port = process.env.PORT || 5000
+const uri = process.env.ATLAS_URI
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port: ${port}`)
+})
+
+mongoose.set('strictQuery', false)
+
+//connect to our db
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB connection established."))
+  .catch((err) => console.log("MongoDB connection failed: ", err)) 
