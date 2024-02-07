cdcdfeb7261a82daa0c5d06305493d0c270ca1ec
jhk742 <jhk742@nyu.edu>
2024-02-07 20:18:06 +0900

    created the getMessages function within messageControllers.js

--- server/Controllers/messageController.js
+++ server/Controllers/messageController.js
@@ -18,4 +18,15 @@ const createMessage = async (req, res) => {
     }
 }
 
+const getMessages = async (req, res) => {
+    const { chatId } = req.params
+    try {
+        const messages = await messageModel.find({chatId})
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+}
+
 module.exports = { createMessage }
