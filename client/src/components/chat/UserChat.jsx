7d25f4025ab2e3939103d3390cdd76cc1ed86662
jhk742 <jhk742@nyu.edu>
2024-02-14 10:21:51 +0900

    the chatlist shows the people with whom the logged-in user has had a conversation with

--- client/src/components/chat/UserChat.jsx
+++ client/src/components/chat/UserChat.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import { useFetchRecipientUser } from '../../hooks/useFetchRecipient'
-
+import { Stack } from 'react-bootstrap'
+import avatar from '../../assets/avatar.svg'
 export default function UserChat( { chat, user }) {
 
+    //data of the receiving user 
     const { recipientUser } = useFetchRecipientUser(chat, user)
 
     return (
-        <div>
-            User Chat
-        </div>
+        <Stack 
+            direction="horizontal" 
+            gap={3} 
+            className="user-card align-items-center p-2 justify-content-between"
+            role="button"
+        >
+                <div className="d-flex">
+                    <div className="me-2">
+                        <img src={avatar} height="35px"></img>
+                    </div>
+                    <div className="text-content">
+                        <div className="name">
+                            {recipientUser?.name}
+                        </div>
+                        <div className="text">
+                            Message
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex flex-column align-items-end">
+                    <div className="date">
+                        12/12/2023
+                    </div>
+                    <div className="this-user-notifications">
+                        2
+                    </div>
+                    <div className="user-online"></div>
+                </div>
+        </Stack>
     )
 }
--- client/src/hooks/useFetchRecipient.js
+++ client/src/hooks/useFetchRecipient.js
@@ -5,18 +5,16 @@ export const useFetchRecipientUser = (chat, user) => {
     const [recipientUser, setRecipientUser] = useState(null)
     const [error, setError] = useState(null)
 
+    //find the other id in the members array that is not equal to the _id of the currently logged-in user
     const recipientId = chat?.members.find((id) => id !== user?._id)
 
     useEffect(() => {
         const getUser = async () => {
             if (!recipientId) return null
-
             const response = await getRequest(`${baseUrl}/users/find/${recipientId}`)
-            
             if (response.error) {
                 return setError(response)
             }
-
             setRecipientUser(response)
         }
 
