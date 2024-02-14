c15f0ce0b50d625d0207e0abc1f6d2040aaed7c3
jhk742 <jhk742@nyu.edu>
2024-02-14 09:46:58 +0900

    template for chats and chatbox created

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ChatContext } from '../context/ChatContext'
+import { Container, Stack } from 'react-bootstrap'
+import UserChat from '../components/chat/UserChat'
+import { AuthContext } from '../context/AuthContext'
 
 export default function Chat() {
+  const { user } = useContext(AuthContext)
+  const { userChats, isUserChatsLoading, userChatsError } = useContext(ChatContext)
+
   return (
-    <div>Chat</div>
+    <div>
+      <Container>
+        {userChats?.length < 1 ? null : 
+        <Stack direction="horizontal" gap={4} className="align-items-start">
+            <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
+              {isUserChatsLoading && <span>Loading chats</span>}
+              {userChats?.map((chat, index) => {
+                return (
+                  <div key={index}>
+                    <UserChat chat={chat} user={user}/>
+                  </div>
+                )
+              })}
+            </Stack>
+            <p>Chatbox</p>
+        </Stack>}
+      </Container>
+    </div>
   )
 }
